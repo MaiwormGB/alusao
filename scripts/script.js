473df8238154a4710fd4eeb00259f73f0bfa4417
Maiworm <biel.maiworm@gmail.com>
2026-05-10 21:24:52 -0300

    atualização de caminho 2

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ input.addEventListener('keypress' , function(event){
         switch(senha){
 
             case senha1:
-                window.location.href = "alusao/pages/catavento.html";
+                window.location.href = "/alusao/pages/catavento.html";
                 break;
 
         }
